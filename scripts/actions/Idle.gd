cfa9e627ed337e655ab4108e5aaf7f215e4ab80b
MattDirty <mtorsell@student.42.fr>
2022-11-19 13:59:47 +0100

    erratically moving Chat

---
+++ scripts/actions/Idle.gd
@@ -0,0 +1,29 @@
+extends Node
+
+var time_between_moves: float = 2.0
+var time_since_last_move: float = 0
+var speed: float = 0
+var velocity: Vector2 = Vector2(0, 0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	time_since_last_move += delta
+	if (time_between_moves <= time_since_last_move):
+		time_since_last_move = 0
+		time_between_moves = randf_range(1, 5)
+		speed = randf_range(-10, 10)
+		velocity = Vector2(300 * speed * delta, 1)
+		if (velocity.x < 0):
+			get_parent().scale.x = -1
+		else:
+			get_parent().scale.x = 1
+	if (velocity.x != 0 && velocity.y != 0):
+		move()
+
+func move():
+	get_parent().move_and_collide(velocity)
+
