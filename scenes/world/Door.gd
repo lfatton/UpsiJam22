01b4a2b4569a0a13f15e9472219b6e8d76405c23
lfatton <louise.fatton@hotmail.fr>
2022-11-19 23:40:53 +0100

    door scene

---
+++ scenes/world/Door.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+var closed = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if (closed):
+				closed = false
+				$InfoDoor.text = "Click to close"
+				$AnimatedSprite.animation = "open"
+				collision_mask = 2
+			else:
+				closed = true
+				$InfoDoor.text = "Click to open"
+				$AnimatedSprite.animation = "closed"
+				collision_mask = 1
+
+
+func _on_mouse_entered():
+	$InfoDoor.show()
+
+
+func _on_mouse_exited():
+	$InfoDoor.hide()
+
