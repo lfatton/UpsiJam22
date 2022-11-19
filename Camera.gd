a4c0f0fd7cc994343e7cb68e0e4ca425d2663e5b
lfatton <louise.fatton@hotmail.fr>
2022-11-19 20:03:17 +0100

    camera clamped to walls

--- Camera.gd
+++ Camera.gd
@@ -23,5 +23,5 @@ func _process(delta):
 		velocity = velocity.normalized() * speed
 
 	position += velocity * delta
-	#position.x = clamp(position.x, 0, screen_size.x)
-	#position.y = clamp(position.y, 0, screen_size.y)
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
