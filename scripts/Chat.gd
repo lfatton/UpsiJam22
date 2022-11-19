c442c5c09b65734b5171f191b7243a71893c9be5
MattDirty <mtorsell@student.42.fr>
2022-11-19 20:07:45 +0100

    randomize chat colors

--- scripts/Chat.gd
+++ scripts/Chat.gd
@@ -6,6 +6,18 @@ extends CharacterBody2D
 var happiness: int = 10
 
 
+func make_material():
+	var new_material = get_material().duplicate()
+	new_material.set_shader_parameter("main_color", Vector4(randf(), randf(), randf(), 1.0))
+	new_material.set_shader_parameter("eye_color", Vector4(randf(), randf(), randf(), 1.0))
+	new_material.set_shader_parameter("ear_color", Vector4(randf(), randf(), randf(), 1.0))
+	return new_material
+	
+
 func _ready():
 	add_child(move_scene.instantiate())
 	add_child(idle_scene.instantiate())
+	
+	$Body.set_material(make_material())
+	$Head.set_material(make_material())
+	$Tail.set_material(make_material())
